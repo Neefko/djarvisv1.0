70237aa56dde427008adec1a439a17fb55128111
Neefko <136015703+Neefko@users.noreply.github.com>
2024-03-28 18:20:32 +0300

    Update proverich.py

--- proverich.py
+++ proverich.py
@@ -5,58 +5,44 @@ import pyttsx3
 import time
 import requests
 import webbrowser
+import os
+import datetime
+import pygame
+import random
+import pyautogui
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 # offline
 engine = pyttsx3.init()
 
+stopped = False
+
+current_time = datetime.datetime.now()
+current_hour = current_time.hour
+
 last_command_time = time.time()
 
-modelbigru = r"C:\Users\79004\vosk-model-ru-0.42"
-modelsmallru = r"C:\Users\79004\vosk-model-small-ru-0.22"
-model = vosk.Model(modelbigru)
+modelbigru = r"vosk-model-ru-0.42"
+modelsmallru = r"vosk-model-small-ru-0.22"
+model = vosk.Model(modelsmallru)
 rec = vosk.KaldiRecognizer(model, 16000)
+
+ope = {
+    0: r'music\open.wav',
+    1: r'music\opencer.wav',
+    2: r'music\loadcer.wav',
+    3: r'music\load.wav'
+}
 
 chunk = 4000
 
+pygame.init()
+
+favorite = 'https://www.youtube.com/watch?v=JFUawBGHAtQ'
 audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=chunk)
-
-
-def say_message(message):
-    print("Голосовой ассистент: " + message)
-    engine.say(message)
-    engine.runAndWait()
-
-
-def do_this_command(message):
-    global last_command_time
-    message = message.lower()
-    if "привет" in message:
-        say_message("Привет, друг!")
-        last_command_time = time.time()
-    elif ("vk" in message or 'вк' in message) and ('открой' or 'открывай'):
-        open_link('https://vk.com')
-        last_command_time = time.time()
-    elif "одноклассники" in message and ('открой' or 'открывай'):
-        open_link('https://ok.ru')
-        last_command_time = time.time()
-    elif ('дискорд' in message or 'discord' in message) and ('открой' or 'открывай'):
-        open_link('https://discord.com/app')
-        last_command_time = time.time()
-    elif "ютуб" in message and ('открой' or 'открывай'):
-        open_link('https://www.youtube.com')
-        last_command_time = time.time()
-    elif ('король и шут' in message or 'короля и шута' in message) and ('лужники' in message or ('в' in message and 'лужниках' in message)) and 'концерт' in message:
-        open_link('https://www.youtube.com/watch?v=JFUawBGHAtQ')
-        last_command_time = time.time()
-    elif "пока" in message:
-        say_message("Пока!")
-        exit()
-    elif 'иван' in message:
-        last_command_time = time.time()
-        pass
-    else:
-        say_message("Команда не распознана!")
+stream = audio.open(format=pyaudio.paInt16, channels=1,
+                    rate=16000, input=True, frames_per_buffer=chunk)
 
 
 def listen_command(timeout=20):
@@ -74,6 +60,18 @@ def listen_command(timeout=20):
                 return text
 
 
+def speek(play):
+    global stopped
+    pygame.mixer.music.load(play)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+def open_speek():
+    rand = random.randint(0,3)
+    speek(ope[rand])
+
+
 def open_link(url):
     try:
         response = requests.get(url)
@@ -85,8 +83,96 @@ def open_link(url):
         say_message("Не удалось открыть ссылку")
 
 
+def say_message(message):
+    print("Голосовой ассистент: " + message)
+    engine.say(message)
+    engine.runAndWait()
+
+
+def do_this_command(message):
+    global last_command_time
+    message = message.lower()
+    if ("привет" in message
+            or "доброе утро" in message
+            or "добрый день" in message
+            or "добрый вечер" in message):
+        if 6 <= current_hour < 12:
+            speek(r'music\goodmorning.wav')
+            print("Голосовой ассистент: Доброе утро")
+        elif 12 <= current_hour < 18:
+            speek(r'music\goodday.wav')
+            print("Голосовой ассистент: Добрый день")
+        else:
+            speek(r'music\goodevening.wav')
+            print("Голосовой ассистент: Добрый вечер")
+        last_command_time = time.time()
+    elif 'блокнот' in message and ('открой' or 'открывай') in message:
+        os.system("C:/Windows/System32/notepad.exe")
+        open_speek()
+        last_command_time = time.time()
+    elif (("vk" in message or ' вк ' in message or 'в контакте' in message)
+          and ('открой' in message or 'открывай' in message)):
+        open_link('https://vk.com')
+        open_speek()
+        last_command_time = time.time()
+    elif "одноклассники" in message and ('открой' or 'открывай'):
+        open_link('https://ok.ru')
+        open_speek()
+        last_command_time = time.time()
+    elif (('дискорд' in message or 'discord' in message
+           or 'игровой мессенджер' in message)
+          and ('открой' or 'открывай')):
+        open_link('https://discord.com/app')
+        open_speek()
+        last_command_time = time.time()
+    elif ("ютуб" in message or "ютюб" in message) and ('открой' or 'открывай'):
+        open_link('https://www.youtube.com')
+        open_speek()
+        last_command_time = time.time()
+    elif (('король и шут' in message or 'короля и шута' in message)
+          and ('лужники' in message or ('в' in message and 'лужниках' in message))
+          and 'концерт' in message):
+        open_link('https://www.youtube.com/watch?v=JFUawBGHAtQ')
+        open_speek()
+        last_command_time = time.time()
+    elif 'протокол' in message and 'программист' in message:
+        open_speek()
+        last_command_time = time.time()
+        os.system(f'start chrome https://beta.theb.ai/home {favorite}')
+        time.sleep(5)
+        pyautogui.hotkey('win', 'right')
+    elif 'новост' in message and 'ковров' in message:
+        open_speek()
+        last_command_time = time.time()
+        url = 'https://vk.com/podslushanokovrov1'
+        driver = webdriver.Chrome()
+        driver.get(url)
+        time.sleep(5)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        post_blocks = soup.find_all('div', {'class': 'wall_post_text'})
+        for index, post in enumerate(post_blocks, start=1):
+            text_inside_block = post.get_text()
+            say_message(f'Пост {index}:\n{text_inside_block}\n')
+            comma = listen_command()  # Слушаем команду
+            message = comma.lower()
+            if 'стоп' in message:
+                say_message("Произошел выход из цикла по команде 'стоп'")
+                break
+        driver.quit()
+        last_command_time = time.time()
+    elif " пока " in message or "пока " in message or "пока" in message:
+        speek(r'music\bye.wav')
+        exit()
+    elif 'валер' in message:
+        last_command_time = time.time()
+        speek(r'music/listencer.wav')
+        print("Голосовой ассистент: Слушаю, сэр")
+    else:
+        say_message("Команда не распознана!")
+
+
 if __name__ == '__main__':
-    say_message('Говорите...')
+    print('Говорите...')
     try:
         waiting_mode = False
         while True:
@@ -98,10 +184,9 @@ if __name__ == '__main__':
                 else:
                     command = listen_command()
                     if command != "":
-                        if "иван" in command:
+                        if "валер" in command:
                             last_command_time = time.time()
                             waiting_mode = False
-                            say_message("Слушаю, сэр")
                             do_this_command(command)
             else:
                 command = listen_command()
